5fd6b309facbdbbdc2966fdce5b1bb01a54db9f5
Leonid Shalupov <leonid@shalupov.com>
2022-05-03 16:23:23 +0200

    space CI

---
+++ .space.kts
@@ -0,0 +1,26 @@
+job("Publish jvm-native-trusted-roots") {
+    startOn {
+        gitPush {
+            enabled = false
+        }
+    }
+
+    container("maven:3-openjdk-11") {
+        env["REPOSITORY_URL"] = "https://jetbrains.team/p/ij/packages/maven/intellij-dependencies"
+
+        shellScript {
+            content = """
+                #!/bin/bash
+                set -x -e -u -o pipefail
+                echo Build and run tests...
+                mvn clean package
+                echo Publish artifacts...
+                mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
+                mvn deploy -s settings.xml \
+                    -DrepositoryUrl=${'$'}REPOSITORY_URL \
+                    -DspaceUsername=${'$'}JB_SPACE_CLIENT_ID \
+                    -DspacePassword=${'$'}JB_SPACE_CLIENT_SECRET
+            """
+        }
+    }
+}
