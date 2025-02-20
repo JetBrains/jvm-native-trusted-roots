e9f5babc5e1b143dfe10a0747800a5d979f23c2e
Tim Jacomb <timjacomb1@gmail.com>
2025-02-20 11:51:59 +0000

    Generate intermediate CA

--- src/test/resources/mock-ca/gen.sh
+++ src/test/resources/mock-ca/gen.sh
@@ -5,5 +5,21 @@ set -e -x -u -o pipefail
 openssl genrsa -out root.key 2048
 openssl req -x509 -sha256 -nodes -extensions v3_ca -key root.key \
   -subj "/O=JETBRAINS/CN=JVM-NATIVE-TRUSTED-ROOTS-MOCK-CA" -days 10950 -out root.crt
-rm -f root.key
 openssl x509 -inform PEM -in root.crt -outform DER -out root.cer
+
+openssl genrsa -out intermediate.key 2048
+openssl req -new -sha256 -nodes -key intermediate.key  \
+  -subj "/O=JETBRAINS/CN=JVM-NATIVE-TRUSTED-ROOTS-MOCK-INTERMEDIATE-CA" -out test-intermediate-ca.csr
+
+openssl x509 -req \
+ -extensions v3_ca \
+ -extfile openssl.cnf \
+ -in test-intermediate-ca.csr \
+ -CA root.crt \
+ -CAkey root.key \
+ -CAcreateserial \
+ -out intermediate-ca.pem \
+ -days 10950 \
+ -sha256
+
+rm -f root.key root.srl test-intermediate-ca.csr intermediate.key
