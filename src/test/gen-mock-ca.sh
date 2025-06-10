1c5e6959b04bf82078488d3e73dd5ea477cb5353
Leonid Shalupov <leonid@shalupov.com>
2025-06-10 21:02:15 +0200

    always generate test certificates to reduce clutter and make it possible to have short-lived certificates

--- src/test/gen-mock-ca.sh
+++ src/test/gen-mock-ca.sh
@@ -2,6 +2,15 @@
 
 set -e -x -u -o pipefail
 
+root="$(cd "$(dirname "$0")"; pwd)"
+dir="$root/resources/mock-ca"
+
+rm -rf "$dir"
+mkdir -p "$dir"
+cd "$dir"
+
+cp "$root/mock-ca.cnf" openssl.cnf
+
 openssl genrsa -out root.key 2048
 openssl req -x509 -sha256 -nodes -extensions v3_ca -key root.key \
   -subj "/O=JETBRAINS/CN=JVM-NATIVE-TRUSTED-ROOTS-MOCK-CA" -days 10950 -out root.crt
