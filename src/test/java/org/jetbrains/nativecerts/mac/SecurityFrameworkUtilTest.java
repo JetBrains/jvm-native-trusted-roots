8aec0a8d5887bda69576200485c445cd50008688
Leonid Shalupov <leonid@shalupov.com>
2025-06-23 18:19:36 +0200

    Revert "uncomment SecurityFrameworkUtilTest.skip_client_certificate as it passes now with the right ssl certificate policy"
    This reverts commit c74636c1104cbf9cc356e1bab8f8078871ecdf9b.

--- src/test/java/org/jetbrains/nativecerts/mac/SecurityFrameworkUtilTest.java
+++ src/test/java/org/jetbrains/nativecerts/mac/SecurityFrameworkUtilTest.java
@@ -6,6 +6,7 @@ import org.jetbrains.nativecerts.NativeCertsSetupLoggingRule;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -83,6 +84,9 @@ public class SecurityFrameworkUtilTest {
     }
 
     @Test
+    @Ignore("Failing due to accepting client cert as a valid, " +
+            "fixed by calling SecPolicyCreateSSL(/* server */ true) " +
+            "instead of SecPolicyCreateSSL(false)")
     public void skip_client_certificate() throws Exception {
         Assume.assumeTrue(isManualTestingEnabled);
 
